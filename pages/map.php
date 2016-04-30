c8c92bc37746cac48e83eac5c57b3d9912e2165b
Amanda Barnes <barnesa8@ferris.edu>
2016-04-29 21:17:19 -0400

    Added html content for Map page

--- pages/map.php
+++ pages/map.php
@@ -26,7 +26,7 @@
             <div class="row gutter">
             	<div class="col">
                 	<h1>Map</h1>
-                	<h3>Secondary Title Name</h3>
+                	<h3>Get Help Navigating</h3>
                 </div>
             </div>
        	</div>
@@ -36,7 +36,108 @@
 					<?php include("sidenav.php");?>
                 </div>
                  <div class="col m10 s10 xs5 orange">
-                    <h1>hey there, this is our kiosk interface.</h1>
+					 <ul>
+						 <li><a href="#">Buildings</a>
+							 <ul>
+							 	<li><a href="#">Dining</a>
+									<ul>
+										<li><a href="#">The Rock Cafe</a></li>
+										<li><a href="#">Quad Cafe</a></li>
+										<li><a href="#">The Market</a></li>
+										<li><a href="#">Starbucks</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Housing</a></li>
+								<ul>
+									<li><a href="#">Residence</a>
+										<ul>
+											<li><a href="#">Bond, North &amp; South</a></li>
+											<li><a href="#">Brophy &amp; McNerney</a></li>
+											<li><a href="#">Clark</a></li>
+											<li><a href="#">Cramer</a></li>
+											<li><a href="#">Hallisy</a></li>
+											<li><a href="#">Henderson</a></li>
+											<li><a href="#">Merill &amp; Travis</a></li>
+											<li><a href="#">Miller</a></li>
+											<li><a href="#">Pickell</a></li>
+											<li><a href="#">Puterbaugh</a></li>
+											<li><a href="#">Vandercook</a></li>
+											<li><a href="#">Ward</a></li>
+										</ul>
+									</li>
+									<li><a href="#">East Campus Suites</a>
+										<ul>
+											<li><a href="#">East Campus 1</a></li>
+											<li><a href="#">East Campus 2</a></li>
+											<li><a href="#">East Campus 3</a></li>
+										<ul>
+									</li>
+									<li><a href="#">Apartments</a>
+										<ul>
+											<li><a href="#">Family Apartments</a></li>
+											<li><a href="#">Single Student Townhouses</a><li>
+										</ul>
+									</li>
+								</ul>
+								<li><a href="#">Academic</a>
+									<ul>
+										<li><a href="#">Alied Health</a></li>
+										<li><a href="#">Alumni Building</a></li>
+										<li><a href="#">Arts and Science Commons</a><li>
+										<li><a href="#">Automotive Center</a></li>
+										<li><a href="#">Business Building</a><li>
+										<li><a href="#">Creative Arts Center</a><li>
+										<li><a href="#">Flite</a><li>
+										<li><a href="#">General Service Building</a><li>
+										<li><a href="#">Granger Building</a><li>
+										<li><a href="#">Hagerman College of Pharmacy</a></li>
+										<li><a href="#">College of Engineering Technology</a></li>
+										<li><a href="#">Institute for Construction Education and Training</a></li>
+										<li><a href="#">Interdisciplinary Resource Center</a><li>
+										<li><a href="#">Knollcrest Commons</a><li>
+										<li><a href="#">Michigan College of Optometry</a></li>
+										<li><a href="#">Music Activites Center</a><li>
+										<li><a href="#">College of Engineering Technology</a><li>
+										<li><a href="#">Power Plant</a></li>
+										<li><a href="#">Prakken Building</a><li>
+										<li><a href="#">Science Building</a></li>
+										<li><a href="#">Science Chiller Complex</a><li>
+										<li><a href="#">Starr Building</a><li>
+										<li><a href="#">Timme Center</a></li>
+										<li><a href="#">West Commons</a></li>
+									</ul>
+								</li>		
+								<ul><a href="#">Entertainment</a>
+									<ul>
+										<li><a href="#">Bookstore</a></li>
+										<li><a href="#">Ewigleben Sports Complex</a></li>
+										<li><a href="#">Katke Golf Course</a><li>
+										<li><a href="#">University Center</a></li>
+										<li><a href="#">Williams Auditorium</a></li>
+										<li><a href="#">Wheeler Pavilion</a></li>
+									</ul>
+								<ul><a href="#">Student Health</a>
+									<ul>
+										<li><a href="#">Public Safety</a></li>
+										<li><a href="#">Raquet Facility</a></li>
+										<li><a href="#">Student Recreation Center</a></li>
+									</ul>
+								
+						 <li><a href="#">Transportation</a>
+						 	<ul>
+								<li><a href="#">Bus Stops</a></li>
+								<li><a href="#">Parking Lots</a></li>
+							</ul>
+						 </li>
+						 <li><a href="#">Professors</a>
+						 	<ul>
+								<li><a href="#">A-F</a></li>
+								<li><a href="#">G-M</a></li>
+								<li><a href="#">N-Q</a></li>
+								<li><a href="#">R-Z</a></li>
+							</ul>
+						 </li>
+					 </ul>
                     <div class="row gutter">
                         <div class="col green">
                             <h1>no</h1>
