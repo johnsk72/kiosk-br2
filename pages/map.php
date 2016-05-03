c190d7ec17c92928f092d492948b49ae853c370d
Amanda Barnes <barnesa8@ferris.edu>
2016-05-03 19:47:44 -0400

    Customizing map

--- pages/map.php
+++ pages/map.php
@@ -286,7 +286,8 @@
 					</div>
 				</div>
 					<div class="col m10 s10 xs5">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.2366562415077!2d-85.48782308441442!3d43.68484317912018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8818d322e4239cd1%3A0xa511e8893935e491!2sFerris+State+University!5e0!3m2!1sen!2sus!4v1462174893466" width="100%" height="900px" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+						<iframe src="https://www.google.com/maps/d/embed?mid=1L7mdvJj2YkcJ9oLsbJcK2690yc4" width="100%" height="900"></iframe>
+					</div>
 				</div>
 			</div>
 			<div class="footer-picture">
