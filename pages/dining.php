c7867d6c985cc884f70fa31418422960485bf1a6
Amanda Barnes <barnesa8@ferris.edu>
2016-05-04 14:44:52 -0400

    pic

--- pages/dining.php
+++ pages/dining.php
@@ -50,7 +50,7 @@
         	</div>
         </div>
 		<div class="footer-picture">
-			<img src="../images/new-filte.png">
+				<img src="../images/new-flite.png">
 		</div>
 	</div>
     <!-- JS and Jquery go here -->
